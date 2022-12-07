6741b675a261906565f0cdd770cdb4c608ec7898
LokaPitel <lokavapitel.udacity@gmail.com>
2022-12-07 23:18:02 +0300

    Now it's class work

--- JordanMRZVIS2.py
+++ JordanMRZVIS2.py
@@ -1,4 +1,83 @@
+from email import iterators
 import numpy as np
+
+class JordanNetwork:
+    def __init__(self, p, L, e):
+        self.e = e
+
+        self.p = p
+        self.L = L
+
+        train_row, train_col = (p + 1), L
+
+        self.W_first = np.random.uniform(size=(train_col + 1, train_col), low=-1, high=1)
+        self.W_second = np.random.uniform(size=(train_col, 1), low=-1, high=1)
+
+    def forward(self, sequence):
+        assert len(sequence) == L
+
+        data = np.empty((1, L + 1))
+        data[0][0:L] = sequence
+        data[0][-1] = Z
+
+        hidden = data @ self.W_first
+        out = hidden @ self.W_second
+
+        return out
+
+    def backpropogation(self, train_matrix, train_etalons):
+        error = 10000000
+        previous_error = 10000
+
+        iteration_number = 0
+        while abs(error - previous_error) > self.e:
+            previous_error = error
+            error = 0
+
+            for input_sequence in range(len(train_matrix)):
+                data = np.empty((1, L + 1))
+                data[0][0:L] = train_matrix[input_sequence]
+                data[0][-1] = Z
+
+                hidden = data @ self.W_first
+                out = hidden @ self.W_second
+
+                delta = out - train_etalons[input_sequence][0]
+
+                W_first_delta = -learning_rate * data.T @ delta @ self.W_second.T
+                W_second_delta = -learning_rate * hidden.T @ delta
+
+                self.W_first += W_first_delta
+                self.W_second += W_second_delta
+
+                
+                data = np.empty((1, L + 1))
+                data[0][0:L] = train_matrix[input_sequence]
+                data[0][-1] = Z
+
+                hidden = data @ self.W_first
+                out = hidden @ self.W_second
+
+                delta = out - train_etalons[input_sequence][0]
+                error += (delta @ delta).item()
+
+            iteration_number += 1
+            print("Epoch #", iteration_number, "Loss: ", error)
+
+
+
+
+def normalize_matrix(matrix):
+    for i_f in range(len(matrix[0])):
+        s = 0
+        for j_f in range(len(matrix)):
+            s += matrix[j_f][i_f] * matrix[j_f][i_f]
+        s = np.sqrt(s)
+
+        for j_f in range(len(matrix)):
+            matrix[j_f][i_f] = matrix[j_f][i_f] / (s + 1)
+
+    return matrix
 
 if __name__ == '__main__':
     sequences = [
@@ -9,8 +88,8 @@ if __name__ == '__main__':
     ]
 
     k = 10 # Count of sequence elements
-    e = 0.09 # Max error
-    learning_rate = 0.0003
+    e = 0.000009 # Max error
+    learning_rate = 0.0007
     momentum = 0.1
     N = 1000000 # Count of epochs
     p = 4 # Window size
@@ -37,45 +116,53 @@ if __name__ == '__main__':
     
     print(train_matrix)
 
-    W_first = np.zeros((train_col + 1, train_col * 2))
-    W_second = np.zeros((train_col * 2, 1))
+    W_first = np.random.uniform(size=(train_col + 1, train_col), low=-1, high=1)
+    W_second = np.random.uniform(size=(train_col, 1), low=-1, high=1)
 
     W_first_delta = 0
     W_second_delta = 0
 
-    error = 0
+    error = 1000000
+    iteration = 0
 
-    for iteration in range(1, N + 1):
-        error = 0
+    #while error > e:
+    #    error = 0
 
-        for input_sequence in range(train_row):
-            date = np.empty((1, train_col + 1))
-            date[0][0:train_col] = train_matrix[input_sequence]
-            date[0][-1] = Z
+    #    iteration += 1
+
+    #    for input_sequence in range(train_row):
+    #        data = np.empty((1, train_col + 1))
+    #        data[0][0:train_col] = train_matrix[input_sequence]
+    #        data[0][-1] = Z
     
-            hidden = date @ W_first
-            out = hidden @ W_second
-            Z = out.item()
+    #        hidden = data @ W_first
+    #        out = hidden @ W_second
+    #        Z = out.item()
 
-            delta = out - train_etalons[input_sequence][0]
+    #        delta = out - train_etalons[input_sequence][0]
 
-            W_first_delta = momentum * W_first_delta - learning_rate * delta * W_second.T
-            W_second_delta = momentum * W_second_delta - learning_rate * delta
+    #        W_first_delta = -learning_rate * data.T @ delta @ W_second.T
+    #        W_second_delta = -learning_rate * hidden.T @ delta
 
-            W_first += W_first_delta
-            W_second += W_second_delta
-            
-            date = np.empty((1, train_col + 1))
-            date[0][0:train_col] = train_matrix[input_sequence]
-            date[0][-1] = Z
+    #        W_first += W_first_delta
+    #        W_second += W_second_delta
+
+    #        data = np.empty((1, train_col + 1))
+    #        data[0][0:train_col] = train_matrix[input_sequence]
+    #        data[0][-1] = Z
     
-            hidden = date @ W_first
-            out = hidden @ W_second
-            delta = out - train_etalons[input_sequence][0]
+    #        hidden = data @ W_first
+    #        out = hidden @ W_second
+    #        delta = out - train_etalons[input_sequence][0]
 
-            error += (delta @ delta).item()
-            #print(train_etalons[input_sequence][0])
-            #print(out)
+    #        error += (delta @ delta).item()
+    #        #print(train_etalons[input_sequence][0])
+    #        #print(out)
 
-        print(f"Iteration #{iteration}: {error}")
+    #    print(f"Iteration #{iteration}: {error}")
 
+    network = JordanNetwork(4, k - 1 - p, 0)
+
+    network.backpropogation(train_matrix, train_etalons)
+
+    print(network.forward([0, 1, 1, 2, 3]))
